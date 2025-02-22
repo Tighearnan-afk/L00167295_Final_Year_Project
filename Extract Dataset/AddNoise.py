d0ff9cd14c051bf195807fa7f3ffc3e08db015d5
Tighearnan-afk <125378590+Tighearnan-afk@users.noreply.github.com>
2025-02-22 20:16:31 +0000

    Init

---
+++ Extract Dataset/AddNoise.py
@@ -0,0 +1,37 @@
+import os
+from os import path
+import skimage as ski
+from PIL import Image
+
+
+if not os.path.exists("Noisy_testing_images"):
+    print("Creating Noisy_testing_images directory...")
+    os.mkdir("Noisy_testing_images")
+
+for testing_Image in os.listdir("Testing_images"):
+    if path.isfile(f"Noisy_testing_images/{testing_Image}.jpg"):
+        print(f"Testing_images/{testing_Image}.jpg already exists")
+        continue
+
+    image = ski.io.imread(os.path.join("Testing_images", testing_Image))
+    image = ski.util.random_noise(image, mode='gaussian', var=0.001)
+    image = ski.util.img_as_ubyte(image)
+    image = Image.fromarray(image).convert("L")
+    image.save(f"Noisy_testing_images/{testing_Image}.jpg",quality=90)
+    print(f"Noisy Testing Image {testing_Image} saved")
+
+if not os.path.exists("Noisy_training_images"):
+    print("Creating Noisy_training_images directory...")
+    os.mkdir("Noisy_training_images")
+
+for training_image in os.listdir("Training_images"):
+    if path.isfile(f"Noisy_training_images/{training_image}.jpg"):
+        print(f"Testing_images/{training_image}.jpg already exists")
+        continue
+
+    image = ski.io.imread(os.path.join("Training_images", training_image))
+    image = ski.util.random_noise(image, mode='gaussian', var=0.001)
+    image = ski.util.img_as_ubyte(image)
+    image = Image.fromarray(image).convert("L")
+    image.save(f"Noisy_training_images/{training_image}.jpg",quality=90)
+    print(f"Noisy Training Image {training_image} saved")
