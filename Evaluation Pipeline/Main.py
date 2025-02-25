3b690c333bf6e348110eba58f2eaa49fc7239990
Tighearnan <125378590+Tighearnan-afk@users.noreply.github.com>
2025-02-25 21:06:03 +0000

    Rough implementation of testing pipeline, it produces an output but adjustments will be required

--- Evaluation Pipeline/Main.py
+++
@@ -1,7 +0,0 @@
-import sklearn as ski
-import jiwer as jw
-
-psnr = ski.metrics.peak_signal_noise_ratio(y_true, y_pred)
-ssim = ski.metrics.ssim(y_true, y_pred)
-wer = jw.wer(y_true, y_pred)
-cer = jw.cer(y_true, y_pred)
---
+++ Testing Pipeline/Main.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import skimage as ski
+import jiwer as jw
+from PIL.Image import Resampling
+import pytesseract
+
+model = tf.keras.models.load_model('../Model/receiptCAE400x400px.keras')
+
+image1 = Image.open('Test Images/1000-receipt.jpg').convert('L').resize((400, 400), Resampling.LANCZOS)
+image1 = np.array(image1)
+image1 = image1.astype('float32') / 255.
+image1 = np.reshape(image1, (400, 400))
+
+noisy_image1 = Image.open('Noisy Test Images/1000-receipt.jpg').convert('L').resize((400, 400), Resampling.LANCZOS)
+noisy_image1 = np.array(noisy_image1)
+noisy_image1 = noisy_image1.astype('float32') / 255.
+noisy_image1 = np.reshape(noisy_image1, (1, 400, 400, 1))
+
+reconstructed_image1 = model.predict(noisy_image1)
+
+reconstructed_image1 = np.reshape(reconstructed_image1, (400, 400))
+
+psnr = ski.metrics.peak_signal_noise_ratio(image1, reconstructed_image1 )
+
+ssim = ski.metrics.structural_similarity(image1, reconstructed_image1, gaussian_weights=True, sigma=1.5, use_sample_covariance=False, data_range=1.0)
+
+print(psnr)
+print(ssim)
+
+reconstructed_image1 = reconstructed_image1.astype('uint8')
+
+prediction = pytesseract.image_to_string(reconstructed_image1)
+text = "Trader Joes"
+
+if prediction == "":
+    prediction = "Output is empty"
+
+wer = jw.wer(prediction, text)
+cer = jw.cer(prediction, text)
+
+print(wer)
+print(cer)
+print("OCR Output: " + prediction)
