6dbe0d60c919dd97c43b40f14a07dde6ada7c4b6
tighearnan-afk <125378590+Tighearnan-afk@users.noreply.github.com>
2025-04-25 20:05:51 +0100

    Removed redundant python script

--- TestImageSizeOCR.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pytesseract
-from PIL.Image import Resampling
-
-#normalImage = Image.open("Testing Pipeline/Test Images/1000-receipt.jpg")
-#print(pytesseract.image_to_string(normalImage))
-
-resizedImage = Image.open("Testing Pipeline/Test Images/1000-receipt.jpg").resize((400,400), Resampling.LANCZOS)
-print(pytesseract.image_to_string(resizedImage))
